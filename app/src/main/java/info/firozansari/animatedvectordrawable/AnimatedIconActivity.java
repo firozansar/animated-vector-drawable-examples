74394e01e3b1619cce5d549b28a99e13a8d31c7a
firozansar <firozansar@gmail.com>
2017-03-19 18:27:50 +0000

    Animated icon activity added

---
+++ app/src/main/java/info/firozansari/animatedvectordrawable/AnimatedIconActivity.java
@@ -0,0 +1,16 @@
+package info.firozansari.animatedvectordrawable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by firoz on 19/03/2017.
+ */
+
+public class AnimatedIconActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animated_icon);
+    }
+}
