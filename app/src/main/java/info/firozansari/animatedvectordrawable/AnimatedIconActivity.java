3700d9f728b4984c66cd1db32af84711155b46c0
firozansar <firozansar@gmail.com>
2017-03-25 20:26:27 +0000

    Bluetooth example added

--- app/src/main/java/info/firozansari/animatedvectordrawable/AnimatedIconActivity.java
+++ app/src/main/java/info/firozansari/animatedvectordrawable/AnimatedIconActivity.java
@@ -37,9 +37,15 @@ public class AnimatedIconActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        AnimatedVectorDrawableCompat avd = AnimatedVectorDrawableCompat.create(AnimatedIconActivity.this, R.drawable.anim_android);
-        AnimatedIcon icon1 = new AnimatedIcon(avd, getString(R.string.android_icon));
+        AnimatedVectorDrawableCompat avd1 = AnimatedVectorDrawableCompat.create(AnimatedIconActivity.this, R.drawable.anim_android);
+        AnimatedVectorDrawableCompat avd2 = AnimatedVectorDrawableCompat.create(AnimatedIconActivity.this, R.drawable.anim_blutooth);
+
+        AnimatedIcon icon1 = new AnimatedIcon(avd1, getString(R.string.android_icon));
+        AnimatedIcon icon2 = new AnimatedIcon(avd2, getString(R.string.bluetooth_icon));
+
+
         animatedIcons.add(icon1);
+        animatedIcons.add(icon2);
 
         mAdapter.setAnimatedIcons(animatedIcons);
 
