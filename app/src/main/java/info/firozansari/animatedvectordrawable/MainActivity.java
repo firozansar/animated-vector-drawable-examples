4d338ef3d1207616462648a78ab78b3472b60b8c
firozansar <firozansar@gmail.com>
2017-04-01 08:57:05 +0100

    Animated Menu items added

--- app/src/main/java/info/firozansari/animatedvectordrawable/MainActivity.java
+++ app/src/main/java/info/firozansari/animatedvectordrawable/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         demos.add(new Demo(this, FillInHeartActivity.class, R.string.fill_in_heart));
         demos.add(new Demo(this, PlayResetActivity.class, R.string.play_to_reset));
         demos.add(new Demo(this, AnimatedIconActivity.class, R.string.animated_icon));
+        demos.add(new Demo(this, AnimatedMenuActivity.class, R.string.animated_menu));
 
 
         recyclerView = (RecyclerView) findViewById(R.id.recycleView);
