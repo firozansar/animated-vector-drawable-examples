6a2e30e70b042d5c6775c569f98de330b703cdd6
Firoz Ansari <firozansar@gmail.com>
2023-01-16 07:43:34 +0000

    Cleaning up

--- app/src/main/java/info/firozansari/animatedvectordrawable/AnimatedIcon.kt
+++
@@ -1,5 +0,0 @@
-package info.firozansari.animatedvectordrawable
-
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
-
-internal class AnimatedIcon(val icon: AnimatedVectorDrawableCompat, val title: String)
